fab8f7839d6efec683f8741eb174301bd1009231
R1KO <r1kobeats@i.ua>
2018-05-25 20:18:26 +0300

    Init Routes
    Init Routes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,16 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => 'web'], function () {
+
+    Route::get('/', 'WorkerController@view')->name('view');
+    Route::post('import', 'WorkerController@import')->name('excel.import');
+    Route::get('export', 'WorkerController@export')->name('excel.export');
+
+	Route::group(['prefix' => 'worker', 'as' => 'worker.'], function () {
+
+		Route::post('add', 'WorkerController@create')->name('add');
+		Route::put('save/{worker}', 'WorkerController@store')->name('save');
+		Route::delete('delete/{worker}', 'WorkerController@delete')->name('delete');
+	});
 });
